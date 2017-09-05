78a2e55217d45d7f6dd3e40aced7e32a1c619d44
Kevin Asistores <kasistores@gmail.com>
2017-09-05 18:41:24 -0400

    assignment3 commit

--- JSONtoMongo.js
+++ JSONtoMongo.js
@@ -8,14 +8,18 @@ var fs = require('fs'),
     Schema = mongoose.Schema, 
     Listing = require('./ListingSchema.js'), 
     config = require('./config');
+    input = require('./listings');
 
 /* Connect to your database */
+mongoose.connect('mongodb://kervinskii:kevin333@ds123124.mlab.com:23124/software-engineering-assignment');
 
 /* 
   Instantiate a mongoose model for each listing object in the JSON file, 
   and then save it to your Mongo database 
  */
-
+for (var i = 0; i < input.length; i++){
+  Listing
+};
 
 /* 
   Once you've written + run the script, check out your MongoLab database to ensure that 
--- ListingSchema.js
+++ ListingSchema.js
@@ -5,11 +5,28 @@ var mongoose = require('mongoose'),
 /* Create your schema */
 var listingSchema = new Schema({
   /* your code here */
+  code: {type: String, required: true, unique: true},
+  name: {type: String, required: true},
+  coordinates: {
+    lattitude: Number,
+    longitude: Number
+  },
+  address: String,
+  updated_at: Date,
+  created_at: Date
+
+
 });
 
 /* create a 'pre' function that adds the updated_at (and created_at if not already there) property */
 listingSchema.pre('save', function(next) {
   /* your code here */
+  now = new Date();
+  this.updated_at = now;
+  if ( !this.created_at ) {
+    this.created_at = now;
+  }
+  next();
 });
 
 /* Use your schema to instantiate a Mongoose model */
