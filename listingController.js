29c4812a6329fe0318f8d681b89c953ca112d709
Kevin Asistores <kasistores@gmail.com>
2017-09-04 22:33:38 -0400

    finished all requirements

--- listingController.js
+++ listingController.js
@@ -1,14 +1,50 @@
 angular.module('listings').controller('ListingsController', ['$scope', 'Listings', 
   function($scope, Listings) {
     $scope.listings = Listings;
-    $scope.detailedInfo = undefined;
+    $scope.detailedInfo = {};
+    $scope.searchResult = "";
+    $scope.newName = "";
+    $scope.newCode = "";
+    $scope.newAddress = "";
 
     /* 
       Implement these functions in the controller to make your application function 
       as described in the assignment spec. 
      */
-    $scope.addListing = function() {};
-    $scope.deleteListing = function(index) {};
-    $scope.showDetails = function(index) {};
+
+    //Add to the list
+    $scope.addListing = function() {
+      console.log("Worked?");
+      $scope.listings.push({
+            "code": $scope.newCode, 
+            "name": $scope.newName,
+            "address": $scope.newAddress}
+      );
+      alert("You've submitted the following information:\ncode: " + $scope.newCode + "\nname: " + $scope.newName + "\naddress: " + $scope.newAddress + "\n");
+      $scope.newName = "";
+      $scope.newCode = "";
+      $scope.newAddress = "";
+      };
+
+
+
+
+    //delete off the list
+    $scope.deleteListing = function(index) {
+      console.log("test test");
+      var location = $scope.listings.indexOf(index);
+      if (location !== -1) {
+        $scope.listings.splice(location, 1);
+      }
+    };
+
+
+
+    //show the details
+    $scope.showDetails = function(index) {
+      console.log("Did this work?")
+      $scope.detailedInfo = index
+    };
+
   }
 ]);
