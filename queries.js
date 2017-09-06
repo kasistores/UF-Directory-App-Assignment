a17db8e4a70dbc37113c2566861120563093e03c
Kevin Asistores <kasistores@gmail.com>
2017-09-06 13:16:18 -0400

    fixed queries

--- queries.js
+++ queries.js
@@ -1,7 +1,9 @@
 /* Fill out these functions using Mongoose queries*/
-var mongoose = require('mongoose');
-var Listings = require('./ListingSchema.js');
-var config = require('./config');
+var mongoose = require('mongoose'),
+    Listing = require('./ListingSchema.js'),
+    config = require('./config');
+
+mongoose.promise = global.promise;
 mongoose.connect(config.db.uri);
 
 var findLibraryWest = function() {
@@ -9,11 +11,13 @@ var findLibraryWest = function() {
     Find the document that contains data corresponding to Library West,
     then log it to the console. 
    */
-  Listings.find({ code: 'LBW' }, function(err, listing) {
+  Listing.find({ code: 'LBW' }, function(err, list) {
     if (err) throw err;
   
     // object of the user
-    console.log(listing);
+    else{
+      console.log(list);
+    }
   });
 };
 
@@ -23,15 +27,11 @@ var removeCable = function() {
     on cable TV. Since we live in the 21st century and most courses are now web based, go ahead
     and remove this listing from your database and log the document to the console. 
    */
-  Listings.find({ code: 'CBL' }, function(err, listing) {
+  Listing.findOneAndRemove({ code: 'CABL' }, function(err) {
     if (err) throw err;
   
-    // delete him
-    user.remove(function(err) {
-      if (err) throw err;
-  
-      console.log('cable successfully deleted!');
-    });
+    // we have deleted the user
+    console.log('User deleted!');
   });
 
 };
@@ -40,22 +40,23 @@ var updatePhelpsLab = function() {
     Phelps Laboratory's address is incorrect. Find the listing, update it, and then 
     log the updated document to the console. 
    */
-  Listings.find({ code: 'PHL' }, function(err, listing) {
+  Listing.findOneAndUpdate({ code: 'PHL' }, { address: '1884-1900 Gale Lemerand Dr, Gainesville, FL 32603' }, function(err, list) {
     if (err) throw err;
-    listing.address = '1884-1900 Gale Lemerand Dr, Gainesville, FL 32603';
+  
     // we have the updated user returned to us
-    console.log(listing);
+    console.log(list);
   });
 };
+
 var retrieveAllListings = function() {
   /* 
     Retrieve all listings in the database, and log them to the console. 
    */
-  Listings.find({}, function(err, listings) {
+  Listing.find({}, function(err, list) {
     if (err) throw err;
   
     // object of all the users
-    console.log(listings);
+    console.log(list);
   });
 };
 
