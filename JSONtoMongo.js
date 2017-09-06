235cbf378bc0325bff2ad18eee66c299c15a3a50
Kevin Asistores <kasistores@gmail.com>
2017-09-06 11:14:59 -0400

    push to database

--- JSONtoMongo.js
+++ JSONtoMongo.js
@@ -7,19 +7,53 @@ var fs = require('fs'),
     mongoose = require('mongoose'), 
     Schema = mongoose.Schema, 
     Listing = require('./ListingSchema.js'), 
-    config = require('./config');
-    input = require('./listings');
+    config = require('./config'),
+    input = require('./listings.json');
 
 /* Connect to your database */
-mongoose.connect('mongodb://kervinskii:kevin333@ds123124.mlab.com:23124/software-engineering-assignment');
+mongoose.connect(config.db.uri);
 
 /* 
   Instantiate a mongoose model for each listing object in the JSON file, 
   and then save it to your Mongo database 
  */
-for (var i = 0; i < input.length; i++){
-  Listing
-};
+for (var i = 0; i < input.entries.length; i++){
+  var newListing;
+  if(input.entries[i].coordinates != null){
+    newListing = Listing ({
+      code: input.entries[i].code,
+      name: input.entries[i].name,
+      coordinates: input.entries[i].coordinates,
+      address: input.entries[i].address
+    });
+  }
+  else{
+    newListing = Listing ({
+      code: input.entries[i].code,
+      name: input.entries[i].name
+    });
+  }
+
+  newListing.save(function(err){
+
+    if (err) throw err;
+    console.log('#' + i + ' User created!');
+
+  });
+
+}
+
+
+
+
+// var myData = fs.readFile('listings.json', 'utf8', function(err, data){
+//     if (err) throw err;
+//     var parsedData = JSON.parse(data);
+//   //look up insertMany
+//     Listing.insertMany(parsedData.entries, function(err, docs){
+//       if(err) throw err;
+//     });
+// });
 
 /* 
   Once you've written + run the script, check out your MongoLab database to ensure that 
--- ListingSchema.js
+++ ListingSchema.js
@@ -5,17 +5,15 @@ var mongoose = require('mongoose'),
 /* Create your schema */
 var listingSchema = new Schema({
   /* your code here */
-  code: {type: String, required: true, unique: true},
+  code: {type: String, required: true},
   name: {type: String, required: true},
   coordinates: {
-    lattitude: Number,
+    latitude: Number,
     longitude: Number
   },
   address: String,
   updated_at: Date,
   created_at: Date
-
-
 });
 
 /* create a 'pre' function that adds the updated_at (and created_at if not already there) property */
